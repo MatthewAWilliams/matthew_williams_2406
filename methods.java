83dbe55aefbc6f181f0f72fca2f2f98b11603fcd
MatthewAWilliams <mawilliams0396@gmail.com>
2016-12-15 14:09:59 -0500

    Added Content

---
+++ methods.java
@@ -0,0 +1,8 @@
+The Java methods methods would be used to help enter and retrive information in the system.
+Java methods include:
+-get (getInventory)
+-set
+-add, addAll
+-remove
+-int
+-boolean
---
+++ tables.sql
@@ -0,0 +1,8 @@
+Some Oracle definitions need to be but in place for my system and for it to function. Some of the table definitions
+would be used to properly access the different parts of the system. The table definitions for this system are:
+-Customer_Name
+-Inventory
+-Employees
+-Locations
+-Suppliers
+-Shippments 
